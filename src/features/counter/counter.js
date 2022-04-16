9eb8a7b21e5d7c2dad877219726dff9287069315
Ngoune Alban <alban.tyrex@gmail.com>
2022-04-16 19:58:19 +0100

    adding simple counter to make sure our actions and reducers works as expected

---
+++ src/features/counter/counter.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import {
+    decrement,
+    increment,
+    selectCount
+} from './counterSlice'
+// import styles from './Counter.module.css'
+
+export function Counter() {
+    const count = useSelector(selectCount)
+    const dispatch = useDispatch()
+    return (
+        <div>
+            <div>
+                <button
+                    // className={styles.button}
+                    aria-label="Increment value"
+                    onClick={() => dispatch(increment())}
+                >
+                    +
+                </button>
+                <span >{count}</span>
+                <button
+                    // className={styles.button}
+                    aria-label="Decrement value"
+                    onClick={() => dispatch(decrement())}
+                >
+                    -
+                </button>
+            </div>
+            {/* omit additional rendering output here */}
+        </div>
+    )
+}
+
+export default Counter;
